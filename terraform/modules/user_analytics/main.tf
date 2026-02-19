f5eab50787e4025a34bb8f7603187061aa4c0038
Jenom <119628596+shehuj@users.noreply.github.com>
2026-02-19 17:11:55 -0600

    Update main.tf

--- terraform/modules/user_analytics/main.tf
+++ terraform/modules/user_analytics/main.tf
@@ -6,6 +6,14 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.0"
+    }
   }
 }
 
