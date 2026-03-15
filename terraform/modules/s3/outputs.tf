a7e81c8d849c0bd288303b0af17fd97d4004a917
Jenom <119628596+shehuj@users.noreply.github.com>
2026-03-14 20:26:50 -0500

    refactored

---
+++ terraform/modules/s3/outputs.tf
@@ -0,0 +1,9 @@
+output "name" {
+  description = "Name of the S3 bucket"
+  value       = aws_s3_bucket.terraform_state.bucket
+}
+
+output "arn" {
+  description = "ARN of the S3 bucket"
+  value       = aws_s3_bucket.terraform_state.arn
+}   
