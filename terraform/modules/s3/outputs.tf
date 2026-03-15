0ce9c57211f4f7d753ecdf31c286917c900ffbbb
Jenom <119628596+shehuj@users.noreply.github.com>
2026-03-15 00:17:48 -0500

    removed s3 module, not needed

--- terraform/modules/s3/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "name" {
-  description = "Name of the S3 bucket"
-  value       = aws_s3_bucket.terraform_state.bucket
-}
-
-output "arn" {
-  description = "ARN of the S3 bucket"
-  value       = aws_s3_bucket.terraform_state.arn
-}   
