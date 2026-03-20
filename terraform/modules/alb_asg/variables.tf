fcce63693d066a86f633058cbac7737d15f2564f
Jenom <119628596+shehuj@users.noreply.github.com>
2026-03-20 11:27:37 -0500

    Dev (#78)
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * fixed cd.yml
    
    * rebase (#49)
    
    * Dev (#45)
    
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * Dev (#46)
    
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * fixed cd.yml
    
    * Dev (#47)
    
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * fixed cd.yml
    
    * Dev (#48)
    
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * fixed cd.yml
    
    * fixed asg issues
    
    * modified ansible playbooks
    
    * removed s3 module, not needed
    
    * Update ghost_db.yml
    
    * fixes
    
    * fixed failures
    
    * Update cd.yml
    
    * Update ghost_db.yml
    
    * fixed domain issue
    
    * updates
    
    * Update main.tf
    
    * fixed resource already exist issue
    
    * optimized ui
    
    * optimized ui
    
    * Update ghost_db.yml
    
    * Update terraform-cleanup.yml
    
    * updated UI
    
    * Update webserver.yml
    
    * modified UI
    
    * updated monitoring stack
    
    * Update main.tf
    
    * Update outputs.tf
    
    * updated prometheus url
    
    * refactored

--- terraform/modules/alb_asg/variables.tf
+++ terraform/modules/alb_asg/variables.tf
@@ -38,9 +38,8 @@ variable "max_size" {
 }
 
 variable "certificate_arn" {
-  description = "ARN of the ACM certificate for HTTPS"
+  description = "ARN of the ACM certificate for HTTPS. Must be provided via the root acm_certificate_arn variable."
   type        = string
-  default     = "arn:aws:acm:us-east-1:615299732970:certificate/6025f24f-a812-41d9-b97a-cce0f4d4426b"
 }
 
 variable "health_check_path" {
