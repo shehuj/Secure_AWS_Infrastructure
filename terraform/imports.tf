2ce6c8f200e3f719d0ae8739d718e3a0acc93f32
Jenom <119628596+shehuj@users.noreply.github.com>
2026-03-20 23:15:32 -0500

    Update imports.tf

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -4,9 +4,4 @@
 # and need to be brought under Terraform management.
 # Remove an import block once the resource is successfully in state.
 
-import {
-  to = module.alb_asg.aws_autoscaling_group.web
-  id = "prod-web-asg"
-}
-
 
