97fcde297c5f31338719351223f9f7e05f207603
Jenom <119628596+shehuj@users.noreply.github.com>
2026-03-20 21:37:54 -0500

    Update imports.tf

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -8,8 +8,3 @@ import {
   to = module.vpc.aws_cloudwatch_log_group.vpc_flow_logs[0]
   id = "/aws/vpc/${var.environment}-flow-logs"
 }
-
-import {
-  to = module.alb_asg.aws_autoscaling_group.web
-  id = "${var.environment}-web-asg"
-}
