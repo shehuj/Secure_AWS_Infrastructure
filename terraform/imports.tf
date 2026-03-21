975de2286975cadcecd5b2e87995857775d48ca0
Jenom <119628596+shehuj@users.noreply.github.com>
2026-03-20 20:57:47 -0500

    Dev (#88)
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * fixed cd.yml
    
    * rebase (#49)
    
    * Dev (#45)
    
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * Dev (#46)
    
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * fixed cd.yml
    
    * Dev (#47)
    
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * fixed cd.yml
    
    * Dev (#48)
    
    * refactored
    
    * fixes
    
    * Update ci.yml
    
    * fixed
    
    * fixed cd.yml
    
    * fixed asg issues
    
    * modified ansible playbooks
    
    * removed s3 module, not needed
    
    * Update ghost_db.yml
    
    * fixes
    
    * fixed failures
    
    * Update cd.yml
    
    * Update ghost_db.yml
    
    * fixed domain issue
    
    * updates
    
    * Update main.tf
    
    * fixed resource already exist issue
    
    * optimized ui
    
    * optimized ui
    
    * Update ghost_db.yml
    
    * Update terraform-cleanup.yml
    
    * updated UI
    
    * Update webserver.yml
    
    * modified UI
    
    * updated monitoring stack
    
    * Update main.tf
    
    * Update outputs.tf
    
    * updated prometheus url
    
    * refactored
    
    * fixed secrets issue
    
    * fixed secret issue
    
    * Update ci.yml
    
    * Update main.tf
    
    * added cloudwatch side car
    
    * Update main.tf
    
    * Update imports.tf
    
    * fixes
    
    * fixed oidc issues
    
    * Update main.tf
    
    * Update main.tf
    
    * fixes

--- terraform/imports.tf
+++ terraform/imports.tf
@@ -3,3 +3,8 @@
 # Add import blocks here only for resources that already exist in AWS
 # and need to be brought under Terraform management.
 # Remove an import block once the resource is successfully in state.
+
+import {
+  to = module.vpc.aws_cloudwatch_log_group.vpc_flow_logs[0]
+  id = "/aws/vpc/${var.environment}-flow-logs"
+}
